102696df839475c51c53975b9431cac7ddd0311f
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2024-01-07 16:40:50 +0200

    Art button first version

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -5,7 +5,7 @@ import styles from './style.module.scss';
 
 interface ButtonProps {
 	// onClick: () => void;
-	children: ReactNode,
+	children: ReactNode;
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -15,9 +15,29 @@ export const Button: React.FC<ButtonProps> = ({
 	return (
 		<button
 			className={styles.button}
-			// onClick={onClick}
+		// onClick={onClick}
 		>
-			{children}
+			<span className={styles.buttonText}>
+				{children}
+			</span>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 31.96 55.75"
+				className={styles.buttonBorderLeft}
+			>
+				<path className={styles.buttonLine} d="M31.95,54.75h-2.19s-8-4.5-8-4.5H1V5.75H31.95" />
+				<path className={styles.buttonArrow} d="M4,8.83s1.23,0,12.22,0L4,20.54s0-11.71,0-11.71Z" />
+			</svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 36.12 56.5"
+				className={styles.buttonBorderRight}
+			>
+				<path className={styles.buttonLine} d="M0,6.5H1.92L10.42,1h24.71V55.5H0" />
+				<path className={styles.buttonArrow} d="M31.46,52.21s-1.23,0-12.22,0l12.22-11.71s0,11.71,0,11.71Z" />
+			</svg>
+			<div className={styles.buttonBorderCenter} />
+
 		</button>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import './styles/reset.scss';
 import './styles/global.scss';
+import './styles/variables.scss';
 
 
 export default function RootLayout({
